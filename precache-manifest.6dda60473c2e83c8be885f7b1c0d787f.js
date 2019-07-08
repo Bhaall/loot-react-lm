9f30fcaac35c6f6ca4b01d8d4d633c7696bc169c
Lee Moberly <lee.moberly@lootcrate.com>
2019-07-08 12:57:42 -0700

    Updates

---
+++ precache-manifest.6dda60473c2e83c8be885f7b1c0d787f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfec241f19a39832d4cd66263e362cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17810eec957e5a11996",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "9315dd4f8b602c575177",
+    "url": "/static/css/main.be4fda6b.chunk.css"
+  },
+  {
+    "revision": "f17810eec957e5a11996",
+    "url": "/static/js/2.c3cb80e6.chunk.js"
+  },
+  {
+    "revision": "9315dd4f8b602c575177",
+    "url": "/static/js/main.b0cdfaab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/;oot-react-lm/precache-manifest.98256797358cf1572955e88493c06ba2.js"
+  "/precache-manifest.6dda60473c2e83c8be885f7b1c0d787f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/;oot-react-lm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
